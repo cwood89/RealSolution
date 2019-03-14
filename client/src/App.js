5985b27dfa59f94145c63ae9d6ece30602bd9634
Kpconway519 <kpconway519@gmail.com>
2019-03-14 19:23:40 -0400

    pushing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import OTLresults from "./pages/OTLresults";
-import CompResults from "./pages/CompResults";
-import About from "./pages/About";
-import Profile from "./pages/Profile"
-import Landing from "./pages/landingPage"
-
-
-=======
 import RouteControl from "./Routing"
->>>>>>> e534696a1b0357d66c3c3306bc18cdee87194d8f
 function App() {
   return (
     <RouteControl />
