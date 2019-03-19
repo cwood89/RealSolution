59594ceda4d19884f3bf50b4ad4d4b2fb550be39
Bingxi Du <bxdu@Bingxis-MacBook-Pro.local>
2019-03-18 22:42:46 -0400

    rewrite sql

--- routes/saleEstTable-api-routes.js
+++ routes/saleEstTable-api-routes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
         "WHERE year_diff in (SELECT MIN(year_diff) FROM sale_finds GROUP BY subject)"
 
   await db.sequelize.query(QUERY,{raw:true}).then( data=> {
-                
+    res.send(data)
                 // if(data){
                 //     for (i=0;i<data.length;i++){
                 //         db.sale_ests.create({
@@ -29,7 +29,7 @@ module.exports = function(app) {
                 // }
         })
 
-   res.send("success")
+   
 
     })
 }
